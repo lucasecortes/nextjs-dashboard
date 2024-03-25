8f3cc755619e14011684205723aabcdba368ef25
lec89 <lucascorts@hotmail.com>
2024-03-25 11:14:47 -0300

    pagination added

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,22 +1,27 @@
 'use client';
 
+import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
-  const pathname = usePathname();
   const { replace } = useRouter();
+  const pathname = usePathname();
 
-  function handleSearch(term: string) {
+  // function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
+    // console.log(`Searching... ${term}`);
+
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
